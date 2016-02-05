189c1b115d43abb1cfc21173ba6b33c4f7b1b014
7x3 D3A7H <s_sheitanov@abv.bg>
2016-02-05 11:06:47 +0200

    New UI

---
+++ JugdeStudentInfo/HtmlJudgeUserResultExtractor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace JugdeStudentInfo
+{
+    public class HtmlJudgeUserResultExtractor
+    {
+        public void GetResults(IEnumerable<string> userNames)
+        {
+            foreach (var name in userNames)
+            {
+                string pattern = @"\b" + name + @"\b(?s)(?:.*?)(<\/tr>)";
+                Regex regex = new Regex(pattern);
+                string html = File.ReadAllText("../../html.html");
+
+                var results = regex.Matches(html);
+
+                foreach (Match match in results)
+                {
+                    string[] points = Regex.Replace(match.Groups[0].Value, @"\D+<td", "")
+                        .Split(new char[] { '>', '<', '\r', '\t', 'd', '\n', 'r', 't', '/', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    using (StreamWriter sw = new StreamWriter(
+                        new FileStream("../../results.txt", FileMode.Append)))
+                    {
+                        sw.Write($"{name} ");
+
+                        foreach (var point in points)
+                        {
+                            sw.Write($"{point} ");
+                        }
+                        sw.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
--- JugdeStudentInfo/JugdeInformationCollector.xaml.cs
+++ JugdeStudentInfo/JugdeInformationCollector.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace JugdeStudentInfo
 {
@@ -22,6 +14,79 @@ namespace JugdeStudentInfo
         public JugdeInformationCollector()
         {
             InitializeComponent();
+        }
+
+        private void AddToFileBox_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".html",
+                Filter = "HTML Files (*.html)|*.html",
+                Multiselect = true
+                
+            };
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string[] fileNamesWithPath = dlg.FileNames;
+
+                foreach (var fileNameWithPath in fileNamesWithPath)
+                {
+                    string fileName =
+                    fileNameWithPath.Substring(fileNameWithPath.IndexOf('\\', fileNameWithPath.Length / 2) + 1);
+                    this.FileBox.Items.Add(fileName);
+                }
+            }
+        }
+
+        private void RemoveFromFileBox_Click(object sender, RoutedEventArgs e)
+        {
+            this.FileBox.Items.Remove(this.FileBox.SelectedItem);
+        }
+
+        private void RemoveFromUserBox_Click(object sender, RoutedEventArgs e)
+        {
+            this.UserBox.Items.Remove(this.UserBox.SelectedItem);
+        }
+
+        private void AddToUserBoxFromTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.UserNameBox.Text))
+            {
+                return;
+            }
+            
+            this.UserBox.Items.Add(this.UserNameBox.Text);
+            this.UserNameBox.Clear();
+        }
+
+        private void UserNameBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Space:
+                    e.Handled = true;
+                    break;
+                case Key.Enter:
+                    ButtonAutomationPeer peer = new ButtonAutomationPeer(this.AddToUserBoxFromTextBox);
+
+                    IInvokeProvider invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+               
+                    invokeProv?.Invoke();
+                    break;
+            }
+        }
+
+        private void AddToUserBoxFromFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveUsersToFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
